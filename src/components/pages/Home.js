00bf96e23aaf6cb129a11179159c526bb7e5b7d5
Carolina <caroonava21@gmail.com>
2024-01-05 12:50:02 -0300

    starting

---
+++ src/components/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import HomeMovies from './Movies/HomeMovies'
+//aquí uno los componentes
+
+function Home() {
+  return (
+    <>
+    <HomeMovies />
+    </>
+  )
+}
+
+export default Home
---
+++ src/components/pages/Movies/HomeMovies.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './HomeMovies.css';
+
+function HomeMovies() {
+  return;
+    
+}
+
+export default HomeMovies;
